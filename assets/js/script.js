8c866aefb7a280c3726a0ccc70619332a355dada
Jeremy <jbjarvis@mac.com>
2023-10-05 16:03:31 -0500

    added comments explaining js code, and added favicon to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var copyBtn = document.querySelector("#copy");
+// create string containing all character for each question
 const charLower = "abcdefghijklmnopqrstuvwxyz"
 const charUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 const charSpecial = "!#$%&'()*+,-./:;<=>?@[^_`{|}~"
 const charNum = "1234567890"
-
+//create empty string to be added to from prompts
 str = "";
 
 function generatePassword() {
@@ -14,6 +15,8 @@ function generatePassword() {
     // var upperYN = confirm("Would you like to add uppercase leters?")
     // var specialYN = confirm("Would you like to add special characters?")
     // var numYN = confirm("Would you like to add numbers?")
+
+    // adds different characters to main str if confirmed
     if (confirm("Would you like to add lowercase letters?")) {
         str += charLower
     } 
@@ -31,6 +34,7 @@ function generatePassword() {
     console.log(str)
     password = "";
 
+    //picks charLength amount of characters from main string at random and returns to password string
     for (let i = 0; i < charLength; ++i) {
         password += str.charAt(Math.floor(Math.random() * str.length + 1));
     }
@@ -48,6 +52,7 @@ function writePassword(password) {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", generatePassword);
+//allows you to copy the generated password with other button
 copyBtn.addEventListener("click", function() {
     navigator.clipboard.writeText(password)
 })
